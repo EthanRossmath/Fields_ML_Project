fdc1b62250eb761b59e379389d2b387698e65f98
EthanRossmath <ethanross981@gmail.com>
2025-06-13 16:53:51 -0400

    TRYING TO CHANGE THE MAIN

--- Main.py
+++ Main.py
@@ -2,7 +2,8 @@ import pandas as pd
 from KMEANSNEW import scan_proc_counts
 from lof_outlier import detect_outlier_processes
 
-
 df = pd.read_csv("synthetic_iphone_latest.csv")
 
 print(scan_proc_counts)
+
+detect_outlier_processes(df)
