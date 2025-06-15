ce52293cf7e0790674265becf9b7ba2da55142c8
Komarex07 <peter_zenz@brown.edu>
2025-06-14 21:13:50 -0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,8 +1,13 @@
 import pandas as pd
 from KMEANSNEW import build_scan_proc_counts
 from lof_outlier import detect_outlier_processes
+from statistical_analysis import analyze_df
 df = pd.read_csv("synthetic_iphone_latest.csv")
 
 print(build_scan_proc_counts(df))
 
 detect_outlier_processes(df)
+
+#Returns a set of anomaly_percentage Devices, based on statistical analysis of procedure count (total - unique)
+#Set create_plots to True, to save graphs for each device in the output_dir.
+analyze_df(df, create_plots=False, output_dir='Statistics',  anomaly_percentage=0.15)
